891820283d5230cf3afac58eb44404bf28c5fd54
MarinaArrieta <arrietamarina12@gmail.com>
2021-06-10 23:08:50 -0300

    create main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/HeaderComponent'
-
+import FooterComponent from './components/FooterComponent'
+import MainComponent from './components/MainComponent'
 function App() {
   return (
     <div className="App">
       <HeaderComponent/>
+      <MainComponent/>
+      <FooterComponent/>
     </div>
   );
 }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,43 @@
+import logan from './logan.jpg';
+const MainComponent = ()=>{
+	return(
+		<main>
+			<section className="conteiner">
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+				<div className="card">
+					<img className="img" src={logan} alt="logan"/>
+					<div className="name">Logan</div>
+				</div>
+			</section>
+		</main>
+		)
+} 
+
+export default MainComponent;
