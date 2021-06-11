442eb7abf99ded6f3198e1dbd3ca89bcb829083f
MarinaArrieta <arrietamarina12@gmail.com>
2021-06-10 23:07:44 -0300

    create footer component

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,36 @@
+const FooterComponent = ()=>{
+	return (
+
+			<footer>
+				<a className="bottom" href="https://github.com/MarinaArrieta">
+		            <abbr className="tooltips" title="GITHUB">
+		                <i className="fab fa-github"></i>
+		            </abbr>
+		        </a>
+		        <a className="bottom" href="https://www.linkedin.com/in/marina-arrieta/">
+		            <abbr className="tooltips" title="LINKEDIN">
+		                <i className="fab fa-linkedin-in">
+		                </i>
+		            </abbr>
+		        </a>
+		        <a className="bottom" href="https://www.instagram.com/marina.arrieta_/">
+		            <abbr className="tooltips" title="INSTAGRAM">
+		                <i className="fab fa-instagram">
+		                </i>
+		            </abbr>
+		        </a>
+		        <a className="bottom" href="developer.html">
+		            <abbr className="tooltips" title="DEVELOPER">
+		                <i className="fas fa-code"></i>
+		            </abbr>
+		        </a>
+		        <a className="bottom" href="https://www.marvel.com/">
+		            <abbr className="tooltips" title="MARVEL">
+		                <i className="fas fa-mask"></i>
+		            </abbr>
+		        </a>
+			</footer>
+		)
+}
+
+export default FooterComponent;
